8893edc373a3a98831d0ddb3d4c60f3982170c59
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-09-17 16:16:02 +0300

    move from readlineSync to promptly

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import readlineSync from 'readline-sync';
+import promptly from 'promptly';
 
 export const standardRoundsCount = 3;
 
-export const run = (description, rounds) => {
+export const run = async (description, rounds) => {
   console.log('Welcome to the Brain Games!');
 
-  const name = readlineSync.question('May I have your name? ');
+  const name = await promptly.prompt('May I have your name? ');
 
   console.log(`Hello, ${name}!`);
   console.log(description);
@@ -13,7 +13,7 @@ export const run = (description, rounds) => {
   // eslint-disable-next-line no-restricted-syntax
   for (const [quest, expectedAnswer] of rounds) {
     console.log(`Question: ${quest}`);
-    const answer = readlineSync.question('Your answer: ');
+    const answer = await promptly.prompt('Your answer: ');
     if (answer.toLowerCase() !== expectedAnswer) {
       console.log(`"${answer}" is wrong answer ;(. Correct answer was "${expectedAnswer}"`);
       console.log(`Let's try again, ${name}!`);
