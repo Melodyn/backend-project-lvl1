f546d8cf567fd501cf40c1faabfcc20102355b7a
Melodyn <samelodyn@gmail.com>
2020-11-18 06:44:47 +0300

    return readline-sync

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readline from 'readline-sync';
 
-export default async () => {
-  const name = await readline.question('May I have your name?');
+export default () => {
+  const name = readline.question('May I have your name?');
   console.log(`Hello, ${name}!`);
 };
